4a77305b50d6b184dd1e1eb0d2590ec37471ca63
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-09-29 16:26:22 -0700

    got the values for wind humidity and temp working on the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@
 var currentCityDay = document.getElementById("chosen-city-day")
 var cityDayOneDate = document.getElementById("day-one")
 var cityDayOne = document.getElementById("card-text-one")
-var cityDayTwoDate = document.getElementById("card-title-two")
+var cityDayTwoDate = document.getElementById("day-two")
 var cityDayTwo = document.getElementById("card-text-two")
-var cityDayThreeDate = document.getElementById("card-title-three")
+var cityDayThreeDate = document.getElementById("day-three")
 var cityDayThree = document.getElementById("card-text-three")
-var cityDayFourDate = document.getElementById("card-title-four")
+var cityDayFourDate = document.getElementById("day-four")
 var cityDayFour = document.getElementById("card-text-four")
-var cityDayFiveDate = document.getElementById("card-title-five")
+var cityDayFiveDate = document.getElementById("day-five")
 var cityDayFive = document.getElementById("card-text-five")
 
 
@@ -62,19 +62,166 @@ function getCityApi() {
       .then(function (deg5data) {
         console.log(deg5data)
         for (var i = 0; i < deg5data.length; i++)
-          console.log(deg5data);
+            console.log(deg5data);
 
-          var list = deg5data.list
+            var list = deg5data.list
 
-          for (var i = 0; i < list.length; i++)
+            for (var i = 0; i < list.length; i++)
 
-          var currentDate = list[4].dt_txt
-          console.log(currentDate);
-          var formatDate = dayjs(currentDate).format("M/DD/YYYY")
-          console.log(formatDate);
+            var dayOneDate = list[5].dt_txt
+            var formatOneDate = dayjs(dayOneDate).format("M/DD/YYYY")
+            cityDayOneDate.textContent = formatOneDate
 
-           cityDayOneDate.textContent = formatDate
-        })  
+            var dayTwoDate = list[13].dt_txt
+            var formatTwoDate = dayjs(dayTwoDate).format("M/DD/YYYY")
+            cityDayTwoDate.textContent = formatTwoDate
+
+            var dayThreeDate = list[21].dt_txt
+            var formatThreeDate = dayjs(dayThreeDate).format("M/DD/YYYY")
+            cityDayThreeDate.textContent = formatThreeDate
+
+            var dayFourDate = list[29].dt_txt
+            var formatFourDate = dayjs(dayFourDate).format("M/DD/YYYY")
+            cityDayFourDate.textContent = formatFourDate
+
+            var dayFiveDate = list[37].dt_txt
+            var formatFiveDate = dayjs(dayFiveDate).format("M/DD/YYYY")
+            cityDayFiveDate.textContent = formatFiveDate
+
+            var dayOneIcon = list[5].weather[0].icon
+            var dayOneIconAdd = document.createElement("img");
+            dayOneIconAdd.setAttribute("src", `https://openweathermap.org/img/wn/${dayOneIcon}.png`);
+            cityDayOneDate.appendChild(dayOneIconAdd);
+
+            var dayTwoIcon = list[13].weather[0].icon
+            var dayTwoIconAdd = document.createElement("img");
+            dayTwoIconAdd.setAttribute("src", `https://openweathermap.org/img/wn/${dayTwoIcon}.png`);
+            cityDayTwoDate.appendChild(dayTwoIconAdd);
+
+            var dayThreeIcon = list[21].weather[0].icon
+            var dayThreeIconAdd = document.createElement("img");
+            dayThreeIconAdd.setAttribute("src", `https://openweathermap.org/img/wn/${dayThreeIcon}.png`);
+            cityDayThreeDate.appendChild(dayThreeIconAdd);
+
+            var dayFourIcon = list[29].weather[0].icon
+            var dayFourIconAdd = document.createElement("img");
+            dayFourIconAdd.setAttribute("src", `https://openweathermap.org/img/wn/${dayFourIcon}.png`);
+            cityDayFourDate.appendChild(dayFourIconAdd);
+
+            var dayFiveIcon = list[37].weather[0].icon
+            var dayFiveIconAdd = document.createElement("img");
+            dayFiveIconAdd.setAttribute("src", `https://openweathermap.org/img/wn/${dayFiveIcon}.png`);
+            cityDayFiveDate.appendChild(dayFiveIconAdd);
+
+            var dayOneTemp = list[5].main.temp
+            var dayOneTempAdd = document.createElement("p");
+            dayOneTempAdd.textContent = ("Temp: " + dayOneTemp + "\xB0")
+            cityDayOneDate.appendChild(dayOneTempAdd);
+
+            var dayTwoTemp = list[13].main.temp
+            var dayTwoTempAdd = document.createElement("p");
+            dayTwoTempAdd.textContent = ("Temp: " + dayTwoTemp + "\xB0")
+            cityDayTwoDate.appendChild(dayTwoTempAdd);
+
+            var dayThreeTemp = list[21].main.temp
+            var dayThreeTempAdd = document.createElement("p");
+            dayThreeTempAdd.textContent = ("Temp: " + dayThreeTemp + "\xB0")
+            cityDayThreeDate.appendChild(dayThreeTempAdd);
+
+            var dayFourTemp = list[29].main.temp
+            var dayFourTempAdd = document.createElement("p");
+            dayFourTempAdd.textContent = ("Temp: " + dayFourTemp + "\xB0")
+            cityDayFourDate.appendChild(dayFourTempAdd);
+
+            var dayFiveTemp = list[37].main.temp
+            var dayFiveTempAdd = document.createElement("p");
+            dayFiveTempAdd.textContent = ("Temp: " + dayFiveTemp + "\xB0")
+            cityDayFiveDate.appendChild(dayFiveTempAdd);
+
+            var dayOneWind = list[5].wind.speed
+            var dayOneWindAdd = document.createElement("p");
+            dayOneWindAdd.textContent = (" " + dayOneWind + " MPH") 
+            dayOneTempAdd.appendChild(dayOneWindAdd);
+
+            var dayTwoWind = list[13].wind.speed
+            var dayTwoWindAdd = document.createElement("p");
+            dayTwoWindAdd.textContent = (" " + dayTwoWind + " MPH") 
+            dayTwoTempAdd.appendChild(dayTwoWindAdd);
+
+            var dayThreeWind = list[21].wind.speed
+            var dayThreeWindAdd = document.createElement("p");
+            dayThreeWindAdd.textContent = (" " + dayThreeWind + " MPH") 
+            dayThreeTempAdd.appendChild(dayThreeWindAdd);
+
+            var dayFourWind = list[29].wind.speed
+            var dayFourWindAdd = document.createElement("p");
+            dayFourWindAdd.textContent = (" " + dayFourWind + " MPH") 
+            dayFourTempAdd.appendChild(dayFourWindAdd);
+
+            var dayFiveWind = list[37].wind.speed
+            var dayFiveWindAdd = document.createElement("p");
+            dayFiveWindAdd.textContent = (" " + dayFiveWind + " MPH") 
+            dayFiveTempAdd.appendChild(dayFiveWindAdd);
+
+            var dayOneHumidity = list[5].main.humidity  
+            var dayOneHumidityAdd = document.createElement("p");
+            dayOneHumidityAdd.textContent = (dayOneHumidity + "%")
+            dayOneWindAdd.appendChild(dayOneHumidityAdd);
+
+            var dayTwoHumidity = list[13].main.humidity  
+            var dayTwoHumidityAdd = document.createElement("p");
+            dayTwoHumidityAdd.textContent = (dayTwoHumidity + "%")
+            dayTwoWindAdd.appendChild(dayTwoHumidityAdd);
+
+            var dayThreeHumidity = list[21].main.humidity  
+            var dayThreeHumidityAdd = document.createElement("p");
+            dayThreeHumidityAdd.textContent = (dayThreeHumidity + "%")
+            dayThreeWindAdd.appendChild(dayThreeHumidityAdd);
+
+            var dayFourHumidity = list[29].main.humidity  
+            var dayFourHumidityAdd = document.createElement("p");
+            dayFourHumidityAdd.textContent = (dayFourHumidity + "%")
+            dayFourWindAdd.appendChild(dayFourHumidityAdd);
+
+            var dayFiveHumidity = list[37].main.humidity  
+            var dayFiveHumidityAdd = document.createElement("p");
+            dayFiveHumidityAdd.textContent = (dayFiveHumidity + "%")
+            dayFiveWindAdd.appendChild(dayFiveHumidityAdd);
     });
-};
+});
+}
 getCityApi();
+
+
+// var fiveDayForecast = {
+//     date: "",
+//     icon: "",
+//     temp: "",
+//     wind: "",
+//     humidity: ""
+// };
+
+// for (var i = 3; i < data[i].length; i += 8){
+//     fiveDayForecast.date = data[i].dt_txt
+//     fiveDayForecast.icon = data[i].weather
+//     fiveDayForecast.temp = data[i].main.temp
+//     fiveDayForecast.wind = data[i].main.wind.speed
+//     vfiveDayForecast.humidity = data[i].main.humidity
+
+//     fiveDayDate = fiveDayForecast.date
+//     fiveDayIcon = fiveDayForecast.icon
+//     fiveDayTemp = fiveDayForecast.temp
+//     fiveDayHumdity = fiveDayForecast.humidity
+
+//     fiveDayForecast[0] = cityDayOne.textContent
+//     fiveDayForecast[1] = cityDayTwo.textContent
+//     fiveDayForecast[2] = cityDayThree.textContent
+//     fiveDayForecast[3] = cityDayFour.textContent
+//     fiveDayForecast[4] = cityDayFive.textContent
+
+//     cityDayOneDate.appendChild(cityDayOne)
+//     cityDayTwoDate.appendChild(cityDayTwo)
+//     cityDayThreeDate.appendChild(cityDayThree)
+//     cityDayFourDate.appendChild(cityDayFour)
+//     cityDayFiveDate.appendChild(cityDayFive)
+// }
