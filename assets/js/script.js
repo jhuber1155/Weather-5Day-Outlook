556d8597303aacaf27fcf50c234fe3eb9ddb5d62
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-09-28 17:57:29 -0700

    Got card with time working and current city and icon to appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // var cityName = document.getElementById("cityNameSearch")
 var currentCityDay = document.getElementById("chosen-city-day")
-var cityDayOneDate = document.getElementById("card-title-one")
+var cityDayOneDate = document.getElementById("day-one")
 var cityDayOne = document.getElementById("card-text-one")
 var cityDayTwoDate = document.getElementById("card-title-two")
 var cityDayTwo = document.getElementById("card-text-two")
@@ -29,19 +29,49 @@ function getCityApi() {
        var cityLatData = cityData[0].lat;
        var cityLongData = cityData[0].lon;
 
-      var requestUrl= "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLatData + "&lon=" + cityLongData + "&appid=9161499c0d3f90ae93e65e2d44573b8e&units=imperial"
+       var requestCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + cityLatData + "&lon=" + cityLongData + "&appid=9161499c0d3f90ae93e65e2d44573b8e&units=imperial"
+
+       fetch(requestCurrentUrl)
+       .then(function (response) {
+        return response.json();
+        })
+        .then(function (degdata) {
+            console.log(degdata)
+            var currentCityName = degdata.name
+            currentCityDay.textContent = currentCityName
+
+            var weatherIcon = degdata.weather
+
+            for (var i = 0; i < weatherIcon.length; i++)
+            var currentWeatherIcon = weatherIcon[0].icon
+            console.log(currentWeatherIcon)
 
 
-      fetch(requestUrl)
+            var currentWeatherAdd = document.createElement("h5")
+            currentWeatherAdd.textContent = currentWeatherIcon
+
+            currentCityDay.append(currentWeatherAdd);
+        })
+
+      var request5DayUrl= "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLatData + "&lon=" + cityLongData + "&appid=9161499c0d3f90ae93e65e2d44573b8e&units=imperial"
+
+      fetch(request5DayUrl)
       .then(function (response) {
         return response.json();
       })
-      .then(function (degdata) {
-        console.log(degdata)
-        for (var i = 0; i < degdata.length; i++)
-          console.log(degdata)
-        });
+      .then(function (deg5data) {
+        console.log(deg5data)
+        for (var i = 0; i < deg5data.length; i++)
+          console.log(deg5data);
 
+          var list = deg5data.list
+
+          for (var i = 0; i < list.length; i++)
+
+          var currentDate = list[4].dt_txt
+          console.log(currentDate);
+          cityDayOneDate.textContent = currentDate
+        })  
     });
 };
 getCityApi();
